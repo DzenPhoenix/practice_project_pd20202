fcde921eea3283f4138754dcab821338201c9da1
Denis <DzenPhoenix@gmail.com>
2021-06-25 13:52:16 +0300

    Refactor associate in Rating model

--- server/src/models/Contest.js
+++ server/src/models/Contest.js
@@ -92,10 +92,7 @@ module.exports = (sequelize, DataTypes) => {
 
   Contest.associate = function (models) {
     Contest.belongsTo(models.Users, { foreignKey: 'userId', targetKey: 'id' });
-    Contest.hasMany(models.Offers, {
-      foreignKey: 'contestId',
-      sourceKey: 'id',
-    });
+    Contest.hasMany(models.Offers, {foreignKey: 'contestId',sourceKey: 'id',});
   };
 
   return Contest;
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,18 +3,11 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-/*
-const configPath = env === 'production' ? path.join(__dirname, '..', '..', '..',
-  'src/server/config/postgresConfig.json') : path.join(__dirname, '..',
-  '/config/postgresConfig.json');
-  */
+
 const configPath = path.join(__dirname, '..', '/config/postgresConfig.json');
 const config = require(configPath)[env];
 const db = {};
-/*
-const sequelize = new Sequelize(config.database, config.username,
-  config.password, config);
-*/
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -41,38 +34,18 @@ fs.readdirSync(__dirname)
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 
-// db['Contests'].belongsTo(db['Users'], {
-//   foreignKey: 'userId',
-//   sourceKey: 'id',
-// });
-// db['Contests'].hasMany(db['Offers'], {
-//   foreignKey: 'contestId',
-//   targetKey: 'id',
-// });
 
-// db['Users'].hasMany(db['Offers'], { foreignKey: 'userId', targetKey: 'id' });
-// db['Users'].hasMany(db['Contests'], { foreignKey: 'userId', targetKey: 'id' });
-// db['Users'].hasMany(db['Ratings'], { foreignKey: 'userId', targetKey: 'id' });
-
-// db['Offers'].belongsTo(db['Users'], { foreignKey: 'userId', sourceKey: 'id' });
-// db['Offers'].belongsTo(db['Contests'], {
-//   foreignKey: 'contestId',
-//   sourceKey: 'id',
-// });
-// db['Offers'].hasOne(db['Ratings'], { foreignKey: 'offerId', targetKey: 'id' });
-
-// дописать
-db['Ratings'].belongsTo(db['Users'], { foreignKey: 'userId', targetKey: 'id' });
+/*db['Ratings'].belongsTo(db['Users'], { foreignKey: 'userId', targetKey: 'id' });
 db['Ratings'].belongsTo(db['Offers'], {
   foreignKey: 'offerId',
   targetKey: 'id',
-});
+});*/
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
